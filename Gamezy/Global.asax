e632743d53115b6a3c9b9793294eb265efac3691
bknie1 <brandonknieriem26@gmail.com>
2018-03-06 17:41:59 -0500

    Add project files.

---
+++ Gamezy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gamezy.MvcApplication" Language="C#" %>
---
+++ Gamezy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gamezy.Startup))]
+namespace Gamezy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
